70b08c31e9e4238ad0ecb2c20482ae2494da0739
Alex Semenov <skarppi@mail.ru>
2019-09-07 11:21:45 +0300

    update

--- types/validators/orders-create-validator.ts
+++ types/validators/orders-create-validator.ts
@@ -1,18 +1,18 @@
-import Joi from "@hapi/joi";
 import {OrdersCreateFields} from "../enums/forms/orders-create";
+import * as yup from "yup";
 
-export const ordersCreateValidationScheme = Joi.object().keys({
-  [OrdersCreateFields.countryId]: Joi.number().required(),
-  [OrdersCreateFields.cryptoCurrencySellId]: Joi.number().required(),
-  [OrdersCreateFields.cryptoCurrencySellPrice]: Joi.string().required(),
-  [OrdersCreateFields.cryptoCurrencyBuyId]: Joi.number().required(),
-  [OrdersCreateFields.cryptoCurrencyBuyPrice]: Joi.string().required(),
-  [OrdersCreateFields.paymentMethodId]: Joi.number().required(),
-  [OrdersCreateFields.bankName]: Joi.string().required(),
-  [OrdersCreateFields.marginProfit]: Joi.string().required(),
-  [OrdersCreateFields.isAutoAdjustTransactionLimit]: Joi.string().required(),
-  [OrdersCreateFields.termsOfTrade]: Joi.string().required(),
-  [OrdersCreateFields.isVerifiedUsersOnly]: Joi.string().required(),
-  [OrdersCreateFields.isTrustedUsersOnly]: Joi.string().required(),
-  [OrdersCreateFields.isIdentifyUsersBeforeContinueTrade]: Joi.string().required(),
-});
+export const ordersCreateValidationScheme = {
+  [OrdersCreateFields.countryId]: yup.number().required(),
+  [OrdersCreateFields.cryptoCurrencySellId]: yup.number().required(),
+  [OrdersCreateFields.cryptoCurrencySellPrice]: yup.string().required(),
+  [OrdersCreateFields.cryptoCurrencyBuyId]: yup.number().required(),
+  [OrdersCreateFields.cryptoCurrencyBuyPrice]: yup.string().required(),
+  [OrdersCreateFields.paymentMethodId]: yup.number().required(),
+  [OrdersCreateFields.bankName]: yup.string().required(),
+  [OrdersCreateFields.marginProfit]: yup.string().required(),
+  [OrdersCreateFields.isAutoAdjustTransactionLimit]: yup.string().required(),
+  [OrdersCreateFields.termsOfTrade]: yup.string().required(),
+  [OrdersCreateFields.isVerifiedUsersOnly]: yup.string().required(),
+  [OrdersCreateFields.isTrustedUsersOnly]: yup.string().required(),
+  [OrdersCreateFields.isIdentifyUsersBeforeContinueTrade]: yup.string().required(),
+};
