2ccf42a649456cf87145b7952ca58a356a47f24e
Alex Semenov <skarppi@mail.ru>
2019-09-07 11:26:43 +0300

    update

--- types/enums/forms/orders-create.ts
+++ types/enums/forms/orders-create.ts
@@ -1,4 +1,4 @@
-export const enum OrdersCreateFields {
+export enum OrdersCreateFields {
   countryId = "countryId",
   cryptoCurrencySellId = "cryptoCurrencySellId",
   cryptoCurrencySellPrice = "cryptoCurrencySellPrice",
